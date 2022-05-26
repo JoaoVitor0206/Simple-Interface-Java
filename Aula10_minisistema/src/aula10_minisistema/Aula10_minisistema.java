5e701158669bddbdedd889a1f7d9f1895a3404de
JoãoVitorRS <100139725+JoaoVitor0206@users.noreply.github.com>
2022-05-26 15:55:58 -0300

    Add files via upload

---
+++ Aula10_minisistema/src/aula10_minisistema/Aula10_minisistema.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula10_minisistema;
+
+/**
+ *
+ * @author JoaoV
+ */
+public class Aula10_minisistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaLogin frame = new TelaLogin();
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+}
---
+++ Aula10_minisistema/src/aula10_minisistema/Carros.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula10_minisistema;
+public class Carros {
+    int ano,modelo;
+    String placa, modelocarro;
+
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public int getModelo() {
+        return modelo;
+    }
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public String getModelocarro() {
+        return modelocarro;
+    }
+    public void setModelocarro(String modelocarro) {
+        this.modelocarro = modelocarro;
+    }      
+}
